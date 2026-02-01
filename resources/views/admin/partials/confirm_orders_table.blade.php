0157fac1ecec00524066c7664fcda06cd592f45e
lma <lma@gmail.com>
2026-02-01 11:56:13 +0630

    abc

--- resources/views/admin/partials/confirm_orders_table.blade.php
+++ resources/views/admin/partials/confirm_orders_table.blade.php
@@ -21,8 +21,7 @@
                         alt="KPay Screenshot"
                         class="rounded border flex-shrink-0"
                         style="height: 40px; width: auto; max-width: 60px; object-fit: cover; cursor: pointer;"
-                        onclick="window.open('{{ asset('adminimages/topups/' . $order->transaction_image) }}', '_blank')"
-                        onerror="this.src='data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2260%22%20height%3D%2240%22%20viewBox%3D%220%200%2060%2040%22%3E%3Crect%20width%3D%2260%22%20height%3D%2240%22%20fill%3D%22%23e9ecef%22%2F%3E%3Ctext%20x%3D%2250%25%22%20y%3D%2250%25%22%20dominant-baseline%3D%22middle%22%20text-anchor%3D%22middle%22%20font-family%3D%22sans-serif%22%20font-size%3D%2210%22%20fill%3D%22%236c757d%22%3ENo%20Img%3C%2Ftext%3E%3C%2Fsvg%3E'">
+                        onclick="window.open('{{ asset('adminimages/topups/' . $order->transaction_image) }}', '_blank')">
                 <a href="{{ asset('adminimages/topups/' . $order->transaction_image) }}"
                     target="_blank"
                     class="btn btn-sm btn-outline-primary flex-shrink-0">
